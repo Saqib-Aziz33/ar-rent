8a2e2a0f6b7a578c78743737e2426ef3f1121bed
Saqib Aziz <83906732+Saqib-Aziz33@users.noreply.github.com>
2021-09-26 08:10:45 -0700

    parallex

--- script/app.js
+++ script/app.js
@@ -1,5 +1,12 @@
 let darkModeBtn = document.getElementById(`dark-mode-btn`);
+const header = document.querySelector('.header');
 
 darkModeBtn.onclick = function(){
     document.body.classList.toggle(`dark-mode`);
 }
+
+
+window.addEventListener('scroll', () => {
+    let offset = window.pageYOffset;
+    header.style.backgroundPositionY = offset * 0.05 + "px"
+})
